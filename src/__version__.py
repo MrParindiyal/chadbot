029ae747fb64fafa49d0b2688a3b6277da2a635c
AP <136710760+MrParindiyal@users.noreply.github.com>
2026-08-30 07:54:13 +0000

    [CHORE] Version bump

--- src/__version__.py
+++ src/__version__.py
@@ -1,5 +1,6 @@
-VERSION_INFO = "2.2.3"
+VERSION_INFO = "2.3"
 
 WHATS_NEW = """
-- [DEV/REFACTOR] Clean and organize helper classes/methods
+- [FEAT] Thread for guessing. Users will now send guesses in the thread of the wordy embed. This will prevent polluting the parent channel and unnecessary notifications for others. 
+- [FEAT] Support for multiple Wordy games! Multiple users can play wordy games simultaneously. However, a user can only have 1 active game at any given time. 
 """
